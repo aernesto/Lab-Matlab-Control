43895d5ff49eac6cabf87fe2ad3912612bebada4
Chris Pizzica <cpizzica@gmail.com>
2018-10-19 16:08:31 -0400

    clean sync Chris' branch

--- tasks/ReversingDots/RDrun.m
+++ tasks/ReversingDots/RDrun.m
@@ -1,18 +1,21 @@
 % Script to run reversing dots task
 function RDrun
 
-DIPSLAY_INDEX = 0;
+DIPSLAY_INDEX = 1;
 USE_REMOTE = false;
-UI = 'dotsReadableEyeMouseSimulator';
+UI = 'dotsReadableHIDKeyboard';
 
 
 % Make the top Node
 topNode = topsTreeNodeTopNode('dotsReversal');
       
-% Use the GUI
+% Use the GUI Error here. Syntax issue?
 % topNode.runGUIname = 'eyeGUI';
 
 % Turn off file saving
+%I had to add the dataFiles inbetween topNode and
+% Filename and create the folders. Not sure what the proper set up is. I'm
+% assuming another syntax/argument error on my part
 topNode.dataFiles.filename = '/Users/lab/GoldLabPsychophysics/Downloaded/Lab-Matlab-Control-eyeDev/tasks/ReversingDots/Data/Subject1';
 
 % Add the screen and text ensemble
@@ -23,7 +26,7 @@ topNode.addDrawables(DIPSLAY_INDEX, USE_REMOTE, false);
 topNode.addReadables(UI);
 
 % Get the dots Reveral Task
-task = topsTreeNodeTaskReversingDots.getStandardConfiguration('NN', 10);
+task = topsTreeNodeTaskReversingDots.getStandardConfiguration('SpecificConfigName', 10);
 
 % Add as child to the maintask. 
 topNode.addChild(task);
